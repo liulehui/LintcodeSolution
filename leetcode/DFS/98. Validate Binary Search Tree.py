1c7f8263a8474ae2292c56168b1f1bdc9d4e5678
刘乐晖 <liulehui@bytedance.com>
2019-06-20 10:20:39 +0800

    20190619 tree

--- leetcode/DFS/98. Validate Binary Search Tree.py
+++ leetcode/DFS/98. Validate Binary Search Tree.py
@@ -1,5 +1,80 @@
 # coding:utf-8
-s = 'abcd'
-dp = [_ for _ in range(len(s))]
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-print(dp)
+# several solutions both O(n) and O(n)
+# solution 1 
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.helper(root)
+        
+    def helper(self,root,lower = float("-inf"),upper = float("inf")):
+        if not root:
+            return True
+        val = root.val
+        if val <= lower or val >= upper:
+            return False
+        
+        if not self.helper(root.right,val,upper):
+            return False
+        if not self.helper(root.left,lower,val):
+            return False
+        return True
+    
+# solution 2  use iteration
+class Solution:
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+            
+        stack = [(root, float('-inf'), float('inf')), ] 
+        while stack:
+            root, lower, upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.right, val, upper))
+            stack.append((root.left, lower, val))
+        return True  
+# solution 3 inorder traverse
+class Solution:
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack, inorder = [], float('-inf')
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            # If next element in inorder traversal
+            # is smaller than the previous one
+            # that's not BST.
+            if root.val <= inorder:
+                return False
+            inorder = root.val
+            root = root.right
+
+        return True 
+
+    
+    
---
+++ leetcode/Tree/236. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,77 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# solution 1 recursive O(N) and O(N)
+# class Solution(object):
+#     def lowestCommonAncestor(self, root, p, q):
+#         """
+#         :type root: TreeNode
+#         :type p: TreeNode
+#         :type q: TreeNode
+#         :rtype: TreeNode
+#         """
+#         if root == None:
+#             return None
+#         if root == p or root == q:
+#             return root
+#         left = self.lowestCommonAncestor(root.left,p,q)
+#         right = self.lowestCommonAncestor(root.right,p,q)
+        
+#         if left != None and right != None:
+#             return root
+#         if left != None:
+#             return left
+#         if right != None:
+#             return right
+#         return None
+class Solution:
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Stack for tree traversal
+        stack = [root]
+
+        # Dictionary for parent pointers
+        parent = {root: None}
+
+        # Iterate until we find both the nodes p and q
+        while p not in parent or q not in parent:
+
+            node = stack.pop()
+
+            # While traversing the tree, keep saving the parent pointers.
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        # Ancestors set() for node p.
+        ancestors = set()
+
+        # Process all ancestors for node p using parent pointers.
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        # The first ancestor of q which appears in
+        # p's ancestor set() is their lowest common ancestor.
+        while q not in ancestors:
+            q = parent[q]
+        return q
+
+        
+        
+        
+        
